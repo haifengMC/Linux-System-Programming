aabba125895d131a0d1c97261d783bb0da13afee
haifengMC <1745502795@qq.com>
2019-08-18 12:59:01 +0800

    哲学家问题升级

--- 09day线程同步/06哲学家问题/main.cpp
+++ 09day线程同步/06哲学家问题/main.cpp
@@ -4,8 +4,58 @@
 
 using namespace std;
 
+#define THREAD_NUM 5
+
+pthread_mutex_t mutexArr[THREAD_NUM], ;
+
+void* threadProc(void* param)
+{
+	int i = *(int*)&param;
+	while (true)
+	{
+		pthread_mutex_lock(&mutexArr[(i + 1) % THREAD_NUM]);
+		//cout << "mutex["<< i <<"]: " << (i + 1) % THREAD_NUM << " ok" << endl;
+		pthread_mutex_lock(&mutexArr[i]);
+		//cout << "mutex[" << i << "]: " << i << " ok" << endl;
+
+		cout << "thread: " << i << " ok" << endl;
+
+		pthread_mutex_unlock(&mutexArr[i]);
+
+		sleep(1);
+
+		pthread_mutex_unlock(&mutexArr[(i + 1) % THREAD_NUM]);
+
+		sleep(1);
+	}
+
+	return NULL;
+}
+
 int main()
 {
+	pthread_t threadArr[THREAD_NUM];
+
+	for (int i = 0; i < THREAD_NUM; i++)
+	{
+		pthread_mutex_init(&mutexArr[i], NULL);
+	}
+	pthread_mutex_lock(&mutexArr[0]);
+	for (int i = 0; i < THREAD_NUM; i++)
+	{
+		pthread_create(threadArr + i, NULL, threadProc, (void*)i);
+		sleep(1);
+	}
+	pthread_mutex_unlock(&mutexArr[0]);
+
+	for (int i = 0; i < THREAD_NUM; i++)
+	{
+		pthread_join(threadArr[i], NULL);
+	}
+	for (int i = 0; i < THREAD_NUM; i++)
+	{
+		pthread_mutex_destroy(&mutexArr[i]);
+	}
 
 
 	return 0;
